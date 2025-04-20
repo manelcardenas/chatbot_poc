f93fbc54cbf07071754c811d90da46a7a10ad9e0
manelcardenas <manelcardenas5@gmail.com>
2025-04-20 19:47:14 +0200

    feat: agents structure defined

---
+++ src/bot/assistants.py
@@ -0,0 +1,55 @@
+from langchain_core.runnables import Runnable
+from langgraph.graph import END
+from langgraph.prebuilt import tools_condition
+
+from src.bot.bot_instance import Assistant
+from src.core.prompts import primary_prompt, recommendation_prompt, spending_prompt
+from src.core.state import ChatbotState
+from src.tools import (
+    CompleteOrEscalate,
+    ToRecommendationAssistant,
+    ToSpendingAssistant,
+    fetch_plan_information,
+    fetch_spending_events,
+    list_supported_plans,
+)
+
+
+class SpendingAssistant(Assistant):
+    """Spending/billing assistant class."""
+
+    def __init__(self, llm: Runnable, name: str = "spending_assistant") -> None:
+        tools = [fetch_spending_events, CompleteOrEscalate]
+        runnable = spending_prompt | llm.bind_tools(tools)
+        super().__init__(runnable=runnable, name=name, tools=tools)
+
+
+class RecommendationAssistant(Assistant):
+    """Plan recommendation assistant class."""
+
+    def __init__(self, llm: Runnable, name: str = "recommendation_assistant") -> None:
+        tools = [list_supported_plans, fetch_plan_information, CompleteOrEscalate]
+        runnable = recommendation_prompt | llm.bind_tools(tools)
+        super().__init__(runnable=runnable, name=name, tools=tools)
+
+
+class PrimaryAssistant(Assistant):
+    """Primary assistant class."""
+
+    def __init__(self, llm: Runnable, name: str = "primary_assistant") -> None:
+        tools = [ToSpendingAssistant, ToRecommendationAssistant]
+        runnable = primary_prompt | llm.bind_tools(tools)
+        super().__init__(runnable=runnable, name=name, tools=tools)
+
+    def route_primary_assistant(self, state: ChatbotState) -> str:
+        route = tools_condition(state)
+        if route == END:
+            return END
+        tool_calls = state["messages"][-1].tool_calls
+        if tool_calls:
+            if tool_calls[0]["name"] == ToSpendingAssistant.__name__:
+                return "enter_spending"
+            elif tool_calls[0]["name"] == ToRecommendationAssistant.__name__:
+                return "enter_recommendation"
+            return "primary_assistant_tools"
+        raise ValueError("Invalid route")
---
+++ src/bot/bot_instance.py
@@ -0,0 +1,38 @@
+from langchain_core.runnables import Runnable, RunnableConfig
+from langgraph.graph import END
+from langgraph.prebuilt import tools_condition
+
+from src.core.state import ChatbotState
+from src.tools import CompleteOrEscalate
+
+
+class Assistant:
+    """Base assistant class which will be inherited by other assistants."""
+
+    def __init__(self, runnable: Runnable, name: str, tools: list) -> None:
+        self.runnable = runnable
+        self.name = name
+        self.tools = tools
+
+    def __call__(self, state: ChatbotState, config: RunnableConfig) -> dict:
+        while True:
+            result = self.runnable.invoke(state)
+
+            if not result.tool_calls and (
+                not result.content or isinstance(result.content, list) and not result.content[0].get("text")
+            ):
+                messages = state["messages"] + [("user", "Respond with a real output.")]
+                state = {**state, "messages": messages}
+            else:
+                break
+        return {"messages": result}
+
+    def route_non_primary_assistants(self, state: ChatbotState) -> str:
+        route = tools_condition(state)
+        if route == END:
+            return END
+        tool_calls = state["messages"][-1].tool_calls
+        did_cancel = any(tc["name"] == CompleteOrEscalate.__name__ for tc in tool_calls)
+        if did_cancel:
+            return "leave_skill"
+        return f"{self.name}_tools"
